34a5a14bff912007aef8370fdca1fd5f99b9d5d8
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-30 18:28:52 +0530

    java basic

---
+++ javabasic.java
@@ -0,0 +1,18 @@
+
+public class javabasic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println(4+3);
+		System.out.println(4*3);
+		int a=3;
+		System.out.println("The value of a is "+a);
+		
+	}
+
+}
