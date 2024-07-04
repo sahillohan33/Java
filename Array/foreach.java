4dea2cd187ac9166bf7ace33cc500079e65ce261
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-04 22:45:58 +0530

    for each loop

---
+++ Array/foreach.java
@@ -0,0 +1,15 @@
+ package Array;
+
+public class foreach {
+
+	public static void main(String[] args) {
+		
+		String names[]= {"Sahil","Lohan","Ankit","Aman"};
+		for(String name:names) {
+			System.out.println("for each is  "+name);
+		}
+
+
+	}
+
+}
