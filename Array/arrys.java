ec2d262fe7788b797e53e97597c614ac6a205bf0
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-04 22:35:43 +0530

    Array and Create array

---
+++ Array/arrys.java
@@ -0,0 +1,38 @@
+package Array;
+
+public class arrys {
+
+	public static void main(String[] args) {
+		
+		/*
+		 
+		int age[];  // declaration
+		age=new int[5]; //allocation
+		
+		// int a[]=new int[5];
+		
+		age[0]=23;
+		age[1]=25;
+		age[2]=27;
+		// System.out.println("Age is : "+age[0]);
+		System.out.println("Age length is : "+age.length);
+		
+		for(int i=0;i<age.length;i++) {
+		System.out.println("Age is : "+age[i]);
+		}
+		
+		*/
+		
+		int marks[]= {95,67,78,89,90};
+		System.out.println(marks[0]);
+		
+		
+		String names[]= {"Sahil","Lohan","Ankit","Aman"};
+		for(int i=0;i<names.length;i++) {
+			System.out.println("My name is : "+names[i]);
+		}
+		
+
+	}
+
+}
