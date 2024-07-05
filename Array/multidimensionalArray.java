c4cef51b32cd63588566f7f3dd3aa0df730a59a0
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-05 18:31:28 +0530

    Multi dimensional Array

---
+++ Array/multidimensionalArray.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class multidimensionalArray {
+
+	public static void main(String[] args) {
+	//	int marks[][]=new int[4][3];
+		
+		
+		
+		/*
+		marks[0][0]=12;
+		marks[0][1]=98;
+		marks[0][2]=35;
+		
+		marks[1][0]=56;
+		marks[1][1]=58;
+		marks[1][2]=89;
+		
+		marks[2][0]=45;
+		marks[2][1]=53;
+		marks[2][2]=39;
+		
+		*/
+		
+		int marks[][]= {
+				{12,98,56},
+				{57,87,67},
+				{48,41,98}
+		};
+		System.out.println(marks[1][2]);
+	}
+
+}
