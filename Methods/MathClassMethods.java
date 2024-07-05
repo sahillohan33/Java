2616bf4389d6901fefa5e458ab732a01b7f5fbfb
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-05 22:32:20 +0530

    learn methods and parameter and math class methods

---
+++ Methods/MathClassMethods.java
@@ -0,0 +1,18 @@
+package Methods;
+
+public class MathClassMethods {
+
+	public static void main(String[] args) {
+		int a=4;
+		int b=10;
+		System.out.println(Math.min(a, b));
+		System.out.println(Math.max(a, b));
+		System.out.println(Math.sqrt(a));
+		System.out.println(Math.pow(a, b));
+		System.out.println(Math.random()*100);
+		System.out.println(Math.floor(5.923546464));
+		System.out.println(Math.ceil(5.1));
+
+	}
+
+}
