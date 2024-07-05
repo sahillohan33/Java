90c401eea504ab55de862b534d1a463dc612c6b0
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-05 19:27:31 +0530

    Learn Methods

---
+++ Methods/learnmethod.java
@@ -0,0 +1,18 @@
+package Methods;
+
+public class learnmethod {
+
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		great();
+		System.out.println("3");
+		System.out.println("4");
+	}
+	static void great() {
+		System.out.println("5");
+		System.out.println("Hello world");
+		System.out.println("6");
+	}
+
+}
