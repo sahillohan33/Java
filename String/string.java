092878bda5119b168e310162e8bfd764bd953fa2
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-06 22:53:14 +0530

    String

--- String/string.java
+++ String/string.java
@@ -4,11 +4,26 @@ public class string {
 
 	public static void main(String[] args) {
 		String name="Sahil";
-		String newName=new String("Sahil Lohan");
+		String sameName="Sahil";
+		String newName=new String("Sahil");
 		
 		System.out.println(name);
 		System.out.println(newName);
-
+		
+		/*
+		if(name==sameName) {
+			System.out.println("Both are same");
+		}
+		if(name==newName) {
+			System.out.println("New Both are same");
+		}
+		else {
+			System.out.println("New Both are not same");
+		}
+*/
+		if(name.equals(newName)) {
+			System.out.println("Both are same");
+		}
 	}
 
 }
