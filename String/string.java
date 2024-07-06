332c7f0c4758e6d6b2f515687b338280b9e6c7d5
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-06 22:27:26 +0530

    Basic String

---
+++ String/string.java
@@ -0,0 +1,14 @@
+package String;
+
+public class string {
+
+	public static void main(String[] args) {
+		String name="Sahil";
+		String newName=new String("Sahil Lohan");
+		
+		System.out.println(name);
+		System.out.println(newName);
+
+	}
+
+}
