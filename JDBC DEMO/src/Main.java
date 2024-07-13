f735370da8d10cc058b1d9fe94f8dba2e2916dd6
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-13 21:42:57 +0530

    select query and database connection

--- JDBC DEMO/src/Main.java
+++ JDBC DEMO/src/Main.java
@@ -1,8 +1,8 @@
 import java.sql.*;
 public class Main {
-   private static final String url="jdbc:mysql://127.0.0.1:3306/?user=root";
+   private static final String url="jdbc:mysql://127.0.0.1:3306/mydb";
    private static final String username="root";
-   private static final String password="";
+   private static final String password="sahil930";
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -16,6 +16,19 @@ public class Main {
 try{
  Connection connection=DriverManager.getConnection(url,username,password);
  Statement statement=connection.createStatement();
+    String query="select * from student";
+    ResultSet resultSet=statement.executeQuery(query);
+    while(resultSet.next()){
+        int id=resultSet.getInt("id");
+        String name=resultSet.getString("name");
+        int age=resultSet.getInt("age");
+        double marks=resultSet.getDouble("marks");
+        System.out.println("Id: "+id);
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Marks: "+marks);
+    }
+
 }
 catch(SQLException e){
     System.out.println(e.getMessage()); // e.printStackTrace();
