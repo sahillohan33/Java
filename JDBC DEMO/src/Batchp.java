719933a01da5f68502cdea4b880cc2271b26f937
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-15 19:05:35 +0530

    Batch processing using statement

---
+++ JDBC DEMO/src/Batchp.java
@@ -0,0 +1,45 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Batchp {
+    private static final String url="jdbc:mysql://127.0.0.1:3306/mydb";
+    private static final String username="root";
+    private static final String password="sahil930";
+
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch(ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            Connection connection=DriverManager.getConnection(url,username,password);
+            Statement statement=connection.createStatement();
+            Scanner scanner=new Scanner(System.in);
+            while(true){
+                System.out.print("Enter Name: ");
+                String name=scanner.next();
+                System.out.print("Enter Age: ");
+                int age=scanner.nextInt();
+                System.out.print("Enter marks: ");
+                double marks=scanner.nextDouble();
+                System.out.print("Enter more data Y/N: ");
+                String choice=scanner.next();
+                String query=String.format("insert into student(name,age,marks) values('%s',%o,%f)",name,age,marks);
+                statement.addBatch(query);
+                if(choice.toUpperCase().equals("N")){
+                    break;
+                }
+            }
+            int[] rowAffected=statement.executeBatch();
+            for(int i=0;i<rowAffected.length;i++){
+                System.out.println("Query: "+i+" not executed successfully");
+            }
+
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
